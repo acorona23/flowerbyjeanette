c3e5efd8249729997ade86c22eefb23ddca2a5e6
acorona23 <alissacorona1@gmail.com>
2022-11-29 10:27:15 -0700

    update

--- main/JS/ham.js
+++ main/JS/ham.js
@@ -1,5 +1,5 @@
 
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.nav')
+const hambutton = document.querySelector('ham');
+const mainnav = document.querySelector('navigation')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
